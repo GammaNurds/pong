c3c70270080b91fa35ac0c9b95fe8c07c22e2456
SHANYUAN <shanyuan55@icloud.com>
2016-10-18 11:15:04 +0200

    heroku

--- src/app/game/player.service.ts
+++ src/app/game/player.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
-import { Player }  from "./player.model";
+import { Player } from '../shared/player.model';
 
 @Injectable()
 export class PlayerService {
