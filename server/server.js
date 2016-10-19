bd37f6e3e15144b18186f7c7646ea85f882ae8fa
SHANYUAN <shanyuan55@icloud.com>
2016-10-19 16:17:30 +0200

    improved stats and server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var path = require("path");
+
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 var port = process.env.PORT || 3002;  //
 var logger = require('morgan');
 var bodyParser = require('body-parser');
@@ -10,13 +13,25 @@ var routes = require('./routes.js');
 
 // middleware
 app.use(logger('dev'));  // morgan
-//app.use(express.static(__dirname + '/dist'));  // production//
 app.use(cors());
 app.use(bodyParser.json());
 
+// database
+var db = process.env.MONGOLAB_URI ||
+"mongodb://admin:admin@ds029655.mlab.com:29655/heroku_gtwrnqdn";
+mongoose.connect(db, function(err) {
+    if (err) {
+        console.log("couldn't connect to mongodb!");
+        console.log(err);
+    } else {
+        console.log("connected to mongodb successfully!");
+    }
+});
 
-require('./database.js');
+// serve ng app
+app.use(express.static(path.resolve(__dirname, '../dist'))); // serve ng app
 
+// serve api routes
 app.use('/api', routes);
 
 app.listen(port, function () {
--- src/app/records/records.component.ts
+++ src/app/records/records.component.ts
@@ -15,6 +15,7 @@ import * as _ from "lodash";
 export class RecordsComponent implements OnInit {
 
     records:Record[];
+    players:Player[];
     stats;
 
     constructor(private recordService:RecordService, private playerService:PlayerService) { }
@@ -22,9 +23,12 @@ export class RecordsComponent implements OnInit {
     ngOnInit() {
         this.stats = {};
 
-        this.recordService.getRecords().then(records => {
-            this.records = records;
-            this.calcStats();
+        // wait for async data, then call stats
+        Promise.all([
+            this.recordService.getRecords(),
+            this.playerService.getPlayers()
+        ]).then(results => {
+            this.calcStats(results[0], results[1]);
         });
     }
 
@@ -32,8 +36,7 @@ export class RecordsComponent implements OnInit {
         return (record.p1Name === playerName && record.p1Sets > record.p2Sets) || (record.p2Name === playerName && record.p1Sets < record.p2Sets)
     }
 
-    calcStats() {
-        var players = ["axel", "florian", "martin", "matthias"];
+    calcStats(records:Record[], players:Player[]):void {
 
         this.stats = {
             wins: [],
@@ -47,23 +50,23 @@ export class RecordsComponent implements OnInit {
 
         for (let player of players) {
 
-            let wins = _.filter(this.records, function(o) {
-                return this.isWinner(player, o);
+            let wins = _.filter(records, o => {
+                return this.isWinner(player.name, o);
             });
 
-            let sweeps = _.filter(this.records, function(o) {
-                return (o.p1Name === player && o.p1Sets > o.p2Sets && o.p2Sets === 0) || (o.p2Name === player && o.p1Sets < o.p2Sets && o.p1Sets === 0);
+            let sweeps = _.filter(records, o => {
+                return (o.p1Name === player.name && o.p1Sets > o.p2Sets && o.p2Sets === 0) || (o.p2Name === player.name && o.p1Sets < o.p2Sets && o.p1Sets === 0);
             });
 
-            let sweepsAgainst = _.filter(this.records, function(o) {
-                return (o.p1Name === player && o.p1Sets < o.p2Sets && o.p1Sets === 0) || (o.p2Name === player && o.p1Sets > o.p2Sets && o.p2Sets === 0);
+            let sweepsAgainst = _.filter(records, o => {
+                return (o.p1Name === player.name && o.p1Sets < o.p2Sets && o.p1Sets === 0) || (o.p2Name === player.name && o.p1Sets > o.p2Sets && o.p2Sets === 0);
             });
 
-            let comebacks = _.filter(this.records, o => {
+            let comebacks = _.filter(records, o => {
                 let isComeback = false;
 
                 // won match
-                if (this.isWinner(player, o)) {
+                if (this.isWinner(player.name, o)) {
                     // lost first two sets
                     if (o.setWinners["1"] !== player && o.setWinners["1"] !== player) {
                         isComeback = true;
@@ -72,11 +75,11 @@ export class RecordsComponent implements OnInit {
                 return isComeback;
             });
 
-            let comebacksAgainst = _.filter(this.records, o => {
+            let comebacksAgainst = _.filter(records, o => {
                 let isComebackAgainst = false;
 
                 // lost match
-                if (!this.isWinner(player, o)) {
+                if (!this.isWinner(player.name, o)) {
                     // won first two sets
                     if (o.setWinners["1"] === player && o.setWinners["1"] === player) {
                         isComebackAgainst = true;
@@ -87,45 +90,41 @@ export class RecordsComponent implements OnInit {
 
             // win streak
             let winStreak = 0;
-            for (let record of this.records.reverse()) {
-                console.log(player);
-                if (this.isWinner(player, record)) {
+            let playedGames = _.filter(records, o => o.p1Name === player.name || o.p2Name === player.name);
+            for (let record of _.reverse(playedGames)) {
+                if (this.isWinner(player.name, record)) {
                     winStreak++;
-                    console.log(winStreak);
-                } else {
-                    console.log("break");
+                } else {  // lost game
                     break;
                 }
             }
 
-
             this.stats.wins.push({
-                playerName: player,
+                playerName: player.name,
                 value: wins.length
             });
             this.stats.winPerc.push({
-                playerName: player,
-                value: wins.length / this.records.length
+                playerName: player.name,
+                value: wins.length / records.length
             });
             this.stats.sweeps.push({
-                playerName: player,
+                playerName: player.name,
                 value: sweeps.length
             });
             this.stats.sweepsAgainst.push({
-                playerName: player,
+                playerName: player.name,
                 value: sweepsAgainst.length
             });
             this.stats.comebacks.push({
-                playerName: player,
+                playerName: player.name,
                 value: comebacks.length
             });
             this.stats.comebacksAgainst.push({
-                playerName: player,
+                playerName: player.name,
                 value: comebacksAgainst.length
             });
-
             this.stats.winStreak.push({
-                playerName: player,
+                playerName: player.name,
                 value: winStreak
             });
         }
