463556a297cf5051333e31ced6e11777870e3c14
SHANYUAN <shanyuan55@icloud.com>
2016-11-03 09:07:50 +0100

    formatting

--- src/app/elo.service.ts
+++ src/app/elo.service.ts
@@ -6,39 +6,39 @@ import { Player } from './shared/player.model';
 @Injectable()
 export class EloService {
 
-  constructor() { }
+    constructor() { }
 
-  calc(winner, loser): any[] {
-      const K = 20;
-      let winner_calc = winner;
-      let loser_calc = loser;
+    calc(winner, loser): any[] {
+        const K = 20;
+        let winner_calc = winner;
+        let loser_calc = loser;
 
-      let EWP_winner = 1 / (1 + (Math.pow(10, ((loser_calc - winner_calc) / 400))));
-      let EWP_loser = 1 - EWP_winner;
+        let EWP_winner = 1 / (1 + (Math.pow(10, ((loser_calc - winner_calc) / 400))));
+        let EWP_loser = 1 - EWP_winner;
 
-      winner = Math.round((winner + K * (1 - EWP_winner)));
-      loser = Math.round((loser + K * (0 - EWP_loser)));
-      return [winner, loser];
-  };
+        winner = Math.round((winner + K * (1 - EWP_winner)));
+        loser = Math.round((loser + K * (0 - EWP_loser)));
+        return [winner, loser];
+    };
 
-  calcELO(records: Record[], players: Player[]): any[] {
-      const STARTPOINTS = 1000;
-      let ranks = [];
+    calcELO(records: Record[], players: Player[]): any[] {
+        const STARTPOINTS = 1000;
+        let ranks = [];
 
-      for (let player of players) {
-          ranks.push({
-              playerName: player.name,
-              elo: STARTPOINTS
-          });
-      }
+        for (let player of players) {
+            ranks.push({
+                playerName: player.name,
+                elo: STARTPOINTS
+            });
+        }
 
-      // calc ELO based on game records
-      for (let record of records) {
-          let p1ELO = _.find(ranks, { playerName: record.p1Name });
-          let p2ELO = _.find(ranks, { playerName: record.p2Name });
+        // calc ELO based on game records
+        for (let record of records) {
+            let p1ELO = _.find(ranks, { playerName: record.p1Name });
+            let p2ELO = _.find(ranks, { playerName: record.p2Name });
 
-          if (record.p1Sets > record.p2Sets) {
-              let newELO = this.calc(p1ELO["elo"], p2ELO["elo"]);
+            if (record.p1Sets > record.p2Sets) {
+                let newELO = this.calc(p1ELO["elo"], p2ELO["elo"]);
 
               // apply new elo to players
               ranks.forEach(function(rank) {
